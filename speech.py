f7fc139a7b1b9750f4fd75afd5b03e9d8e56d3ec
Narayan Bandodker <46126206+nb-programmer@users.noreply.github.com>
2020-03-02 14:41:32 +0530

    Added speech synth wrapper

---
+++ speech.py
@@ -0,0 +1,23 @@
+import pyttsx3
+
+class Speech:
+    BEGUN ="Paper collection begun"
+    AUTOON="automatic mode activated"
+    AUTOOFF="automatic mode disabled"
+    ON="powered on" 
+    OFF="Shutting down"
+
+    def __init__(self):
+        self.engine = pyttsx3.init()
+
+    def speak(self,text):
+        self.engine.say(text)
+        self.engine.runAndWait()
+
+if __name__ == "__main__":
+    s=Speech()
+    s.speak(Speech.BEGUN)
+    s.speak(Speech.AUTOON)
+    s.speak(Speech.AUTOOFF)
+    s.speak(Speech.ON)
+    s.speak(Speech.OFF)
